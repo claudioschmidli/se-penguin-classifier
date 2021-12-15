150012e16c1c7316b758827a1e5dc01966f923a2
Claudio Schmidli <claudio.schmidli@gmail.com>
2021-12-15 21:19:42 +0100

    Added styling to app

--- API/flask/app.py
+++ API/flask/app.py
@@ -3,10 +3,12 @@ import numpy as np
 from flask import Flask, request, render_template
 import pickle
 import logging
+import os
 
-
+currentdir = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(currentdir, "../../model/model.pkl")
 app = Flask(__name__)
-model = pickle.load(open("../../model/model.pkl", "rb"))
+model = pickle.load(open(model_path, "rb"))
 
 # setup logging
 logging.basicConfig(filename="error.log", level=logging.ERROR)
@@ -26,18 +28,39 @@ def predict():
     try:
         float_features = [float(x) for x in request.form.values()]
     except ValueError:
-        app.logger.info("A error happend", exc_info=True)
+        app.logger.error("A error happend", exc_info=True)
         error_Message = "bitte Masse mit Gleitkommazahlen definieren"
         return render_template("inputFeatures.html", prediction_text=error_Message)
-    print("features", float_features)
+
+    app.logger.info("features", float_features, exc_info=True)
+
+    # print("features", float_features)
     features = [np.array(float_features)]
-    print("features1", features)
+
+    # print("features1", features)
     prediction = model.predict(features)
 
-    output = "Penguin is of the species Adelie"
+    output = f"""
+    <div id=positive_result>
+        <p>Penguin is of the species Adelie</p>
+        <ul>
+            <li>Culmen length: {float_features[0]} mm</li>
+            <li>Culmen depth: {float_features[1]} mm</li>
+        </ul>
+        <img src="static/adelie.png" alt="Yes it is Adelie">
+    </div>
+    """
     if not prediction[0]:
-        output = "Penguin is not of the species Adelie"
-
+        output = f"""
+        <div id=negative_result>
+            <p>Penguin is not of the species Adelie</p>
+            <ul>
+                <li>Culmen length: {float_features[0]} mm</li>
+                <li>Culmen depth: {float_features[1]} mm</li>
+            </ul>
+            <img src="static/no_adelie.png" alt="No it is not Adelie">
+        </div>
+        """
     return render_template("inputFeatures.html", prediction_text=output)
 
 
